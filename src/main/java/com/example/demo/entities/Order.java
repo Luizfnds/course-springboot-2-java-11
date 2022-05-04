b1ef3c15109af24145808aee3b5c1cceb3276cb1
Luizfnds <luizeduardofnds@gmail.com>
2022-05-04 14:59:56 -0300

    OrderItem, many-to-many association with extra attributes

--- src/main/java/com/example/demo/entities/Order.java
+++ src/main/java/com/example/demo/entities/Order.java
@@ -2,18 +2,15 @@ package com.example.demo.entities;
 
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 import com.example.demo.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 @Table(name = "tb_order")
@@ -32,6 +29,9 @@ public class Order implements Serializable{
 	@ManyToOne
 	@JoinColumn(name = "client_id")
 	private User client;
+
+    @OneToMany(mappedBy = "id.order")
+    private Set<OrderItem> items = new HashSet<>();
 
 	public Order(){
 	}
@@ -77,6 +77,10 @@ public class Order implements Serializable{
 	public void setClient(User client) {
 		this.client = client;
 	}
+
+    public Set<OrderItem> getItems(){
+        return items;
+    }
 
 	@Override
 	public int hashCode() {
